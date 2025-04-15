286d71c13d67698640fd243d2d0f5f60f885d107
Abhishek Jacob Santhosh <abhisheksanthosh45@gmail.com>
2025-04-15 12:58:19 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,28 +1,31 @@
 import unittest
-import pytest
 
-def func1(a,b):
-    c=a+b
+
+def func1(a, b):
+    c = a + b
     return c
 
 def func2(arr):
     arr.sort()
     return arr
 
-def test_func1():
-    print(func1(2, 3))  
-    print(func1(-2, 3)) 
-    print(func1(0, 0))  
-    print(func1(-2, -3))
 
-def test_func2():
-    print(func2([3, 2, 1]))       
-    print(func2([1, 2, 3]))       
-    print(func2([5, 1, 9, 2]))   
-    print(func2([3]))             
-    print(func2([1, 1, 1]))       
+class TestFunctions(unittest.TestCase):
 
+    def test_func1(self):
+        
+        self.assertEqual(func1(2, 3), 5) 
+        self.assertEqual(func1(-2, 3), 1)  
+        self.assertEqual(func1(0, 0), 0)   
+        self.assertEqual(func1(-2, -3), -5) 
 
-test_func1()
-test_func2()
+    def test_func2(self):
+        
+        self.assertEqual(func2([3, 2, 1]), [1, 2, 3])  
+        self.assertEqual(func2([1, 2, 3]), [1, 2, 3])  
+        self.assertEqual(func2([5, 1, 9, 2]), [1, 2, 5, 9])  
+        self.assertEqual(func2([3]), [3])  
+        self.assertEqual(func2([1, 1, 1]), [1, 1, 1])  
 
+if __name__ == '__main__':
+    unittest.main()
